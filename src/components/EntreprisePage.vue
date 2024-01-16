a4d0f2a444473519a8b590bfb8409831ca8b17e7
Martin Erpicum <martin@erpicum.net>
2024-01-16 14:33:27 +0100

    Update EntreprisePage.vue

--- src/components/EntreprisePage.vue
+++ src/components/EntreprisePage.vue
@@ -1,24 +1,86 @@
 <template>
   <div class="">
-    {{ beNumber }}
     <div v-if="data_loaded">
       <div class="md:flex">
         <div class="mt-4 md:mt-0 md:ml-6">
+          <h1 class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">
+            {{ listDenominations }}
+          </h1>
           <div class="uppercase tracking-wide text-sm text-indigo-600 font-bold">
             {{ company.EnterpriseNumberBE }}
           </div>            
+        
+          <hr>
+          <h2 class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">
+            Adresse(s)
+          </h2>
+          <div>
+            <ul>
+              <li
+                v-for="address in company.Addresses"
+                :key="address"
+              >
+                {{ address.StreetFR }} {{ address.HouseNumber }} {{ address.Box }}
+                <br>
+                {{ address.Zipcode }} {{ address.MunicipalityFR }}
+              </li>
+            </ul>
+          </div>
 
-          {{ company.Denominations }}
+          <h2 class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">
+            Contacts
+          </h2>
+          <div>
+            <ul>
+              <li
+                v-for="contact in company.Contacts"
+                :key="contact"
+              >
+                {{ contact.ContactType }}: 
+                <span v-if="contact.ContactType == 'EMAIL'">
+                  <a
+                    :href="'mailto:' + contact.Value"
+                  >{{ contact.Value }}</a>
+                </span>
+                <span v-if="contact.ContactType == 'WEB'"><a
+                  :href="'https://' + contact.Value"
+                >{{ contact.Value }}</a>
+                </span>
+                <span v-if="contact.ContactType == 'TEL'"><a
+                  :href="'tel:' + contact.Value"
+                >{{ contact.Value }}</a>
+                </span>
+              </li>
+            </ul>
+          </div>
+          <h2 class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">
+            Establishments
+          </h2>
+          <div>
+            <ul>
+              <li
+                v-for="est in company.Establishments"
+                :key="est"
+              >
+                {{ est.EnterpriseNumber }}: {{ est.StartDate }}
+              </li>
+            </ul>
+          </div>
 
           <hr>
-
-          {{ company.Addresses }}
-          <hr>
-          {{ company.Contacts }}
-          <hr>
-          {{ company.Establishments }}
-          <hr>
-          {{ company.Activities }}
+          <h2 class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">
+            Activities
+          </h2>
+          <div>
+            <ul>
+              <li
+                v-for="nace in company.Activities"
+                :key="nace"
+              >
+                {{ nace.activity }} {{ nace.NaceCode }}: {{ nace.labels }}
+              </li>
+            </ul>
+          </div>
           <hr>
           {{ company.StatusLabel }}
           <hr>
@@ -28,7 +90,6 @@
           <hr>
           {{ company.JuridicalFormCACLabel }}
 
-          {{ company.ExternalLinks }}
 
           <BarChart :data="company.SubsidiesPerYearForChart" />
           <ul>
@@ -36,7 +97,22 @@
               v-for="subsidy in company.Subsidies"
               :key="subsidy"
             >
-              {{ subsidy.Year }}: {{ subsidy.MinistreName }} {{ subsidy.Compétence }} {{ subsidy.AmountInEuros }}
+              {{ subsidy.Year }}: {{ subsidy.MinistreName }} {{ subsidy.Compétence }} {{ subsidy.AmountInEuros }} €
+            </li>
+          </ul>
+
+          <h2>
+            Liens externes
+          </h2>
+          <ul class="list-disc">
+            <li
+              v-for="key, link in company.ExternalLinks"
+              :key="link"
+            >
+              <a
+                :href="key.href"
+                target="_blank"
+              >{{ key.service_name }}</a>
             </li>
           </ul>
         </div>
@@ -67,6 +143,12 @@ export default {
       company: {},
       data_loaded: false
     };
+  },
+  computed: {
+    // join with a comma as separator
+    listDenominations: function () {
+      return this.company.Denominations.map(denomination => denomination.Denomination).join(' / ');
+    }
   },
   mounted () {
     this.getSubsidies();
