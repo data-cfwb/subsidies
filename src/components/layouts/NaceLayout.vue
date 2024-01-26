cf2f65859e3c1364aafa201386f4adc44975fb42
Martin Erpicum <martin@erpicum.net>
2024-01-26 04:35:15 +0100

    add beautiful indicator ❤️

--- src/components/layouts/NaceLayout.vue
+++ src/components/layouts/NaceLayout.vue
@@ -1,43 +1,63 @@
 <template>
   <div v-if="data_loaded">
     <HeaderPartial
-      :title="'Statistiques pour le secteur ' + insights.code.Description"
+      :title="insights.code.Description"
+      subtitle="Statistiques pour le secteur"
     />
     <main>
       <div class="mx-auto max-w-7xl px-6 lg:px-8 mt-10">
         <div>
           <div class="px-4 sm:px-0">
-            <h3 class="text-base font-semibold leading-7 text-gray-900">
-              {{ insights.code.Description }}
-            </h3>
-            <p class="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
+            <p class="mt-1 max-w-2xl text-md leading-6 text-gray-700">
               Détails sur le secteur "{{ insights.code.Description }}" en Belgique référencé par la Banque Carrefour des Enterprises (BCE) sous le code NACE {{ insights.code.Code }} ({{ insights.code.Category }}).
             </p>
           </div>
-          <div class="mt-6 border-t border-gray-100">
-            <dl class="divide-y divide-gray-100">
-              <div class="px-2 py-2 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-0">
-                <dt class="text-sm font-medium leading-12 text-gray-900">
-                  Nombre d'enterprises dans le secteur en tant que code principal ou secondaire
+
+          <div class="mx-auto max-w-7xl px-6 lg:px-8 mt-10">
+            <dl
+        
+              class="mt-5 grid grid-cols-1 divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow md:grid-cols-3 md:divide-x md:divide-y-0"
+            >
+              <div
+
+                class="px-4 py-5 sm:p-6"
+              >
+                <dt class="text-base font-normal text-gray-900">
+                  Organisations dans le secteur en tant que code principal ou secondaire
                 </dt>
-                <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0 text-right">
-                  {{ insights.enterprises_total }}
+                <dd class="mt-1 flex items-baseline justify-between md:block lg:flex">
+                  <div class="flex items-baseline text-2xl font-semibold text-dark-blue">
+                    {{ insights.enterprises_total }} <span class="ml-2 text-sm font-medium text-gray-500">organisations</span>
+                  </div>
                 </dd>
               </div>
-              <div class="px-4 py-6 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-0">
-                <dt class="text-sm font-medium leading-6 text-gray-900">
-                  Nombre d'enterprises dans le secteur subventionnées par la Fédération Wallonie-Bruxelles
+
+              <div
+
+                class="px-4 py-5 sm:p-6"
+              >
+                <dt class="text-base font-normal text-gray-900">
+                  Organisations du secteur subventionnées par la FWB
                 </dt>
-                <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0 text-right">
-                  {{ insights.enterprises_subsidized }} ({{ Math.round(insights.enterprises_subsidized / insights.enterprises_total * 100) }}%)
+                <dd class="mt-1 flex items-baseline justify-between md:block lg:flex">
+                  <div class="flex items-baseline text-2xl font-semibold text-dark-blue">
+                    {{ insights.enterprises_subsidized }}  <span class="ml-2 text-sm font-medium text-gray-500">({{ Math.round(insights.enterprises_subsidized / insights.enterprises_total * 100) }}%)</span>
+                  </div>
                 </dd>
               </div>
-              <div class="px-4 py-6 sm:grid sm:grid-cols-2 sm:gap-4 sm:px-0">
-                <dt class="text-sm font-medium leading-6 text-gray-900">
-                  Montant total des subventions sur le secteur {{ insights.code.Description }} (en €)
+
+              <div
+
+                class="px-4 py-5 sm:p-6"
+              >
+                <dt class="text-base font-normal text-gray-900">
+                  Montant total des subventions sur le secteur (en €)
                 </dt>
-                <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0 text-right">
-                  {{ $filters.formatToEuros(insights.subsidies_total) }}
+                <dd class="mt-1 flex items-baseline justify-between md:block lg:flex">
+                  <div class="flex items-baseline text-2xl font-semibold text-dark-blue">
+                    {{ $filters.formatToEuros(insights.subsidies_total) }}
+                    <span class="ml-2 text-sm font-medium text-gray-500" />
+                  </div>
                 </dd>
               </div>
             </dl>
@@ -46,6 +66,9 @@
 
     
         <div class="mt-6 border-t border-gray-100">
+          <h2 class="text-base font-semibold leading-6 text-gray-900 uppercase">
+            Liste des organisations subventionnées
+          </h2>
           <dl class="divide-y divide-gray-100">
             <div
               v-for="subsidy in insights.all_enterprises"
