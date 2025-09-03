ecbc4232e0f90943ffd0bc167f5117dabd897109
Martin Erpicum <martin.erpicum@cfwb.be>
2025-09-03 15:00:23 +0200

    fix: inclusion of ul in p element

--- src/components/FooterModule.vue
+++ src/components/FooterModule.vue
@@ -2,13 +2,13 @@
   <footer class="bg-white">
     <div class="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
       <div class="mt-8 md:order-1 md:mt-0">
-        <p class="text-left text-xs leading-5 text-gray-500">
+        <div class="text-left text-xs leading-5 text-gray-500">
           Réalisé sous licence libre. <a href="https://github.com/data-cfwb/subsidies">Code</a><br>
           <ul class="text-left text-xs leading-5 text-gray-500">          
             <li><a href="http://www.federation-wallonie-bruxelles.be/index.php?id=apropos">À propos de la FW-B</a></li>
             <li><a href="http://www.federation-wallonie-bruxelles.be/index.php?id=contact">Contacter la FW-B</a></li>
           </ul>
-        </p>
+        </div>
       </div>
       <div class="flex justify-center space-x-6 md:order-2">
         <img
@@ -20,13 +20,13 @@
         >
       </div>
       <div class="flex justify-center space-x-6 md:order-3">
-        <p class="text-right text-xs leading-5 text-gray-500">
+        <div class="text-right text-xs leading-5 text-gray-500">
           2023 - {{ new Date().getFullYear() }}
           <ul class="text-right text-xs leading-5 text-gray-500">          
             <li><a href="/declaration-sur-l-accessibilite">Déclaration d'accessibilité</a></li>
             <li><a href="http://www.federation-wallonie-bruxelles.be/index.php?id=portail_legal">Mentions légales et vie privée</a></li>
           </ul>
-        </p>
+        </div>
         <img
           src="https://raw.githubusercontent.com/data-cfwb/charte-graphique/main/pastilles_SVG/logo_FWB_coul.svg"
           alt="Fédération Wallonie-Bruxelles"
